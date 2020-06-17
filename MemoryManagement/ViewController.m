5e7a32b298d8fe1cad0a1b45477b6ca4577fe2e5
Paul Solt <PaulSolt@gmail.com>
2020-06-17 13:01:44 -0400

    Memory leak from lack of release calls to lower retain count before viewDidLoad finishes

--- MemoryManagement/ViewController.m
+++ MemoryManagement/ViewController.m
@@ -21,11 +21,35 @@
 {
     [super viewDidLoad];
 
-    // TODO: Disable ARC in settings
+    // Disable ARC in settings
     
     NSLog(@"Hi");
     
-}
+//    NSString *test = @"string literal"; // special allocation in readonly memory
+    
+    // alloc/init == retain
+    // retainCount = 1
+    // retain -> retainCount = 2
+    // release -> retainCount = 1
+    // release -> retainCount = 0 (Memory is destoyed ... i.e.: recycled for reuse)
+    
+    // dynamically allocating the string using alloc/init
+    NSString *name = [[NSString alloc] initWithString:@""]; // name: 1
+    
+    // retain/release are only available with ARC disabled (MRC - manual reference counting)
+    [name retain]; // name: 2
+    [name retain]; // name: 3
+    
+    // What will happen with name?
+    
+    // If I don't release name (down to zero retain count), we have a memory leak
+    // no other in-memory app can use this location
+} // end of scope, we lose the name reference
 
+
+- (void)updateViews {
+    // name doesn't exist here, out of scope, because we don't have a property or instance variable
+    //name // Error: Use of undeclared identifier 'name'
+}
 
 @end
